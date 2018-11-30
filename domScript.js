20e122e6b75094c2593c189a276f77aaea34c649
Joyce Cho <joyjoycho@gmail.com>
2018-11-29 21:13:43 -0800

    change list with script

--- domScript.js
+++ domScript.js
@@ -1,4 +1,11 @@
 const changeColor = document.querySelector('.red-square');
 changeColor.addEventListener("click", function() {
     changeColor.style.backgroundColor = 'limegreen';
-})
+});
+
+const changeList = document.querySelectorAll('.list-change');
+    for (let i = 0; i < changeList.length; i++) {
+        const currentEle = changeList[i];
+
+        currentEle.innerText = "modified by Javascript";
+    }
